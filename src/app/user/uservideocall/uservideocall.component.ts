18fae3f05513a7a3b736252c033cb4f64742b052
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-07-27 21:12:06 +0530

    video size

--- src/app/user/uservideocall/uservideocall.component.ts
+++ src/app/user/uservideocall/uservideocall.component.ts
@@ -59,7 +59,7 @@ export class UservideocallComponent  implements OnInit{
   private createRoom(): void {
     this.options = {
       roomName: this.room,
-      width: 1500,
+      width: 1000,
       height: 700,
       configOverWrite: {
         proJoinPage: false
